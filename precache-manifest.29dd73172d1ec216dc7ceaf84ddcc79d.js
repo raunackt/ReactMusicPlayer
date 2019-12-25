a03285e15219a6dbbe200e3bf9c3588aa74c5f83
37471927 <le-raunack@users.noreply.github.com>
2019-12-25 21:22:31 +0530

    Updates

---
+++ precache-manifest.29dd73172d1ec216dc7ceaf84ddcc79d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa3b3fb72d9f7b863f53eea6678e88c",
+    "url": "/ReactMusicPlayer/index.html"
+  },
+  {
+    "revision": "129ffccc392042007c79",
+    "url": "/ReactMusicPlayer/static/css/main.ac7f9c79.chunk.css"
+  },
+  {
+    "revision": "45cc7ad02ce813565cb5",
+    "url": "/ReactMusicPlayer/static/js/2.01fc9c07.chunk.js"
+  },
+  {
+    "revision": "1fdd8477b82dbd0dc30ff62b40004fea",
+    "url": "/ReactMusicPlayer/static/js/2.01fc9c07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "129ffccc392042007c79",
+    "url": "/ReactMusicPlayer/static/js/main.b9ab52d8.chunk.js"
+  },
+  {
+    "revision": "a27e4740e00afbd6869f",
+    "url": "/ReactMusicPlayer/static/js/runtime-main.f6a8ff41.js"
+  },
+  {
+    "revision": "c3fc4b84556f2fb93c40a3772d8054e0",
+    "url": "/ReactMusicPlayer/static/media/pos-1.c3fc4b84.jpg"
+  },
+  {
+    "revision": "1d7abb9cb62b2764b25657a4fd249cc4",
+    "url": "/ReactMusicPlayer/static/media/pos-10.1d7abb9c.jpeg"
+  },
+  {
+    "revision": "67e141ce77685eb6cf48ef70541f23f3",
+    "url": "/ReactMusicPlayer/static/media/pos-2.67e141ce.jpg"
+  },
+  {
+    "revision": "38aaaeae77ea807780bf9b85633ee1b7",
+    "url": "/ReactMusicPlayer/static/media/pos-3.38aaaeae.jpg"
+  },
+  {
+    "revision": "1db19f2885da813945ffa77509c2ec83",
+    "url": "/ReactMusicPlayer/static/media/pos-4.1db19f28.png"
+  },
+  {
+    "revision": "bf72f6cfb36054b6a051940d49083213",
+    "url": "/ReactMusicPlayer/static/media/pos-5.bf72f6cf.jpeg"
+  },
+  {
+    "revision": "08ee26991a1dd0437f1be2c16b976ae8",
+    "url": "/ReactMusicPlayer/static/media/pos-6.08ee2699.jpg"
+  },
+  {
+    "revision": "ba5078df4c16289f39ba1c74dcf83dc3",
+    "url": "/ReactMusicPlayer/static/media/pos-7.ba5078df.jpg"
+  },
+  {
+    "revision": "70de8b7ca71b5c2abf93c101d0318a6d",
+    "url": "/ReactMusicPlayer/static/media/pos-8.70de8b7c.jpeg"
+  },
+  {
+    "revision": "39800881f25dbb48ef51d510fbdb7b9a",
+    "url": "/ReactMusicPlayer/static/media/pos-9.39800881.jpg"
+  },
+  {
+    "revision": "c8d594eadd2adb849b40424ae84a980e",
+    "url": "/ReactMusicPlayer/static/media/song-1.c8d594ea.mp3"
+  },
+  {
+    "revision": "63cc0a849da3671271a9ccfb7c335d54",
+    "url": "/ReactMusicPlayer/static/media/song-10.63cc0a84.mp3"
+  },
+  {
+    "revision": "e175412e95b9aeba27a71b47937e0fad",
+    "url": "/ReactMusicPlayer/static/media/song-2.e175412e.mp3"
+  },
+  {
+    "revision": "78d8e9ad461f0f452cfd9104624cd2d2",
+    "url": "/ReactMusicPlayer/static/media/song-3.78d8e9ad.mp3"
+  },
+  {
+    "revision": "780462c2501503afc5cadc18e61f00db",
+    "url": "/ReactMusicPlayer/static/media/song-4.780462c2.mp3"
+  },
+  {
+    "revision": "80276ac2e9b80cdde919b3c40b4a79cc",
+    "url": "/ReactMusicPlayer/static/media/song-5.80276ac2.mp3"
+  },
+  {
+    "revision": "c8541ce63795c612189559d9060d4579",
+    "url": "/ReactMusicPlayer/static/media/song-6.c8541ce6.mp3"
+  },
+  {
+    "revision": "b7303a025fee4c52c32f735833b2bbfa",
+    "url": "/ReactMusicPlayer/static/media/song-7.b7303a02.mp3"
+  },
+  {
+    "revision": "c3709ca6b7327f1652926543507b9adc",
+    "url": "/ReactMusicPlayer/static/media/song-8.c3709ca6.mp3"
+  },
+  {
+    "revision": "a2c312ccd319aea41e671c4049600a94",
+    "url": "/ReactMusicPlayer/static/media/song-9.a2c312cc.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Music-Player/precache-manifest.ab1170afa675ebf90ede3215e472dda0.js"
+  "/ReactMusicPlayer/precache-manifest.29dd73172d1ec216dc7ceaf84ddcc79d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Music-Player/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactMusicPlayer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
