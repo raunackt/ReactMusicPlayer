31a8a2a2fee0515aa235d32eee8809ca7ee4f70d
37471927 <le-raunack@users.noreply.github.com>
2019-12-25 21:47:36 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Player from "./components/Player";
-import songOne from "./songs/song-1.mp3";
-import songTwo from "./songs/song-2.mp3";
-import songThree from "./songs/song-3.mp3";
-import songFour from "./songs/song-4.mp3";
-import songFive from "./songs/song-5.mp3";
-import songSix from "./songs/song-6.mp3";
-import songSeven from "./songs/song-7.mp3";
-import songEight from "./songs/song-8.mp3";
-import songNine from "./songs/song-9.mp3";
-import songTen from "./songs/song-10.mp3";
-import songOnePoster from "./img/posters/pos-1.jpg";
-import songTwoPoster from "./img/posters/pos-2.jpg";
-import songThreePoster from "./img/posters/pos-3.jpg";
-import songFourPoster from "./img/posters/pos-4.png";
-import songFivePoster from "./img/posters/pos-5.jpeg";
-import songSixPoster from "./img/posters/pos-6.jpg";
-import songSevenPoster from "./img/posters/pos-7.jpg";
-import songEightPoster from "./img/posters/pos-8.jpeg";
-import songNinePoster from "./img/posters/pos-9.jpg";
-import songTenPoster from "./img/posters/pos-10.jpeg";
 import SongsList from "./components/SongsList";
 import playButton from "./img/buttons/play.png";
 import pauseButton from "./img/buttons/pause.png";
+import ResourcesArray from "./components/ResourcesArray";
 
 function App() {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
@@ -33,68 +14,7 @@ function App() {
   const [currentSongTime, setCurrentSongTime] = useState(0);
   const [totalTime, setTotalTime] = useState(0);
 
-  const songsList = [
-    {
-      song: songOne,
-      songName: "Adventure",
-      songPoster: songOnePoster,
-      songCredit: "bensounds.com"
-    },
-    {
-      song: songTwo,
-      songName: "Creative Minds",
-      songPoster: songTwoPoster,
-      songCredit: "bensounds.com"
-    },
-    {
-      song: songThree,
-      songName: "Energy",
-      songPoster: songThreePoster,
-      songCredit: "bensounds.com"
-    },
-    {
-      song: songFour,
-      songName: "Epic",
-      songPoster: songFourPoster,
-      songCredit: "bensounds.com"
-    },
-    {
-      song: songFive,
-      songName: "Happy Rock",
-      songPoster: songFivePoster,
-      songCredit: "bensounds.com"
-    },
-    {
-      song: songSix,
-      songName: "Inspire",
-      songPoster: songSixPoster,
-      songCredit: "bensounds.com"
-    },
-    {
-      song: songSeven,
-      songName: "Once Again",
-      songPoster: songSevenPoster,
-      songCredit: "bensounds.com"
-    },
-    {
-      song: songEight,
-      songName: "Relaxing",
-      songPoster: songEightPoster,
-      songCredit: "bensounds.com"
-    },
-    {
-      song: songNine,
-      songName: "Sexy",
-      songPoster: songNinePoster,
-      songCredit: "bensounds.com"
-    },
-    {
-      song: songTen,
-      songName: "Tomorrow",
-      songPoster: songTenPoster,
-      songCredit: "bensounds.com"
-    }
-  ];
+  const songsList = ResourcesArray();
 
   const handlePlayOrPause = () => {
     setIsPlaying(!isPlaying);
@@ -154,6 +74,8 @@ function App() {
   const songSrc = songsList[currentSongIndex].song;
   const songNameSrc = songsList[currentSongIndex].songName;
   const posterSrc = songsList[currentSongIndex].songPoster;
+
+  useEffect(() => console.log(currentSongIndex), [currentSongIndex]);
 
   return (
     <main>
---
+++ src/components/ResourcesArray.jsx
@@ -0,0 +1,87 @@
+import songOne from "../songs/song-1.mp3";
+import songTwo from "../songs/song-2.mp3";
+import songThree from "../songs/song-3.mp3";
+import songFour from "../songs/song-4.mp3";
+import songFive from "../songs/song-5.mp3";
+import songSix from "../songs/song-6.mp3";
+import songSeven from "../songs/song-7.mp3";
+import songEight from "../songs/song-8.mp3";
+import songNine from "../songs/song-9.mp3";
+import songTen from "../songs/song-10.mp3";
+import songOnePoster from "../img/posters/pos-1.jpg";
+import songTwoPoster from "../img/posters/pos-2.jpg";
+import songThreePoster from "../img/posters/pos-3.jpg";
+import songFourPoster from "../img/posters/pos-4.png";
+import songFivePoster from "../img/posters/pos-5.jpeg";
+import songSixPoster from "../img/posters/pos-6.jpg";
+import songSevenPoster from "../img/posters/pos-7.jpg";
+import songEightPoster from "../img/posters/pos-8.jpeg";
+import songNinePoster from "../img/posters/pos-9.jpg";
+import songTenPoster from "../img/posters/pos-10.jpeg";
+
+function ResourcesArray() {
+  return [
+    {
+      song: songOne,
+      songName: "Adventure",
+      songPoster: songOnePoster,
+      songCredit: "bensounds.com"
+    },
+    {
+      song: songTwo,
+      songName: "Creative Minds",
+      songPoster: songTwoPoster,
+      songCredit: "bensounds.com"
+    },
+    {
+      song: songThree,
+      songName: "Energy",
+      songPoster: songThreePoster,
+      songCredit: "bensounds.com"
+    },
+    {
+      song: songFour,
+      songName: "Epic",
+      songPoster: songFourPoster,
+      songCredit: "bensounds.com"
+    },
+    {
+      song: songFive,
+      songName: "Happy Rock",
+      songPoster: songFivePoster,
+      songCredit: "bensounds.com"
+    },
+    {
+      song: songSix,
+      songName: "Inspire",
+      songPoster: songSixPoster,
+      songCredit: "bensounds.com"
+    },
+    {
+      song: songSeven,
+      songName: "Once Again",
+      songPoster: songSevenPoster,
+      songCredit: "bensounds.com"
+    },
+    {
+      song: songEight,
+      songName: "Relaxing",
+      songPoster: songEightPoster,
+      songCredit: "bensounds.com"
+    },
+    {
+      song: songNine,
+      songName: "Sexy",
+      songPoster: songNinePoster,
+      songCredit: "bensounds.com"
+    },
+    {
+      song: songTen,
+      songName: "Tomorrow",
+      songPoster: songTenPoster,
+      songCredit: "bensounds.com"
+    }
+  ];
+}
+
+export default ResourcesArray;
